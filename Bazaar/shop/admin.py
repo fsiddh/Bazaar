b1d6debfab1a23520c7b153152f7ceb826025a27
fsiddh <jasonshaw546@gmail.com>
2020-08-07 00:40:03 +0530

    updates

--- Bazaar/shop/admin.py
+++ Bazaar/shop/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Product
 
 # Register your models here.
+from .models import Product, Contact
+
 admin.site.register(Product)
+admin.site.register(Contact)
