c4d0ca44c09b8497dbdabbba04d0ad052e379d77
fsiddh <jasonshaw546@gmail.com>
2020-08-06 09:31:26 +0530

    Updates

--- Bazaar/shop/urls.py
+++ Bazaar/shop/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from shop import views
+from . import views
 
 urlpatterns =[
-    path('', views.index, name='home')
+    path('', views.index, name='ShopHome'),
+    path('about/', views.about, name='AboutUs'),
+    path('contact/', views.contact, name='ContactUs'),
+    path('tracker/', views.tracker, name='TrackingStatus'),
+    path('productview/', views.productView, name='ProductView'),
+    path('search/', views.search, name='Search'),
+    path('checkout', views.checkout, name='Checkout')
 ]
--- Bazaar/shop/views.py
+++ Bazaar/shop/views.py
@@ -4,3 +4,21 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'shop/index.html')
+    
+def about(request):
+    return HttpResponse("a")
+
+def contact(request):
+    return HttpResponse("c")
+
+def tracker(request):
+    return HttpResponse("r")
+
+def productView(request):
+    return HttpResponse("p")
+
+def search(request):
+    return HttpResponse("s")
+
+def checkout(request):
+    return HttpResponse("c")
