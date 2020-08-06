e5b74b151f1224f49c263ead0fe554d64d82ed0b
fsiddh <jasonshaw546@gmail.com>
2020-08-06 16:52:38 +0530

    updates

--- Bazaar/shop/views.py
+++ Bazaar/shop/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
+from math import ceil
 
 # Create your views here.
 
 def index(request):
-    product = Product.objects.all() 
-    data = {
-        'Product': product
+    products = Product.objects.all() 
+    n = len(products)
+    nSlides = n//4 + ceil((n/4) - (n//4))
+    param = {
+        'product': products
     }
-    return render(request, 'shop/index.html', data)
+    return render(request, 'shop/index.html', param)
     
 def about(request):
     return render(request, 'shop/about.html')
