0861a561b4f8141f7915306dd339f8ce2bae3897
fsiddh <jasonshaw546@gmail.com>
2020-08-07 12:24:23 +0530

    updtes

--- MyAwesomeCart-master/mac/settings.py
+++ MyAwesomeCart-master/mac/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9h_#wy*6)%#ug3-uv@7xlryan5a36rqe^j5a$-i0@fo9szu=%n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bazaar-django.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
