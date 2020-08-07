bc258be0261ee7118e26eebe18f92d59bcd8faaa
fsiddh <jasonshaw546@gmail.com>
2020-08-07 11:15:42 +0530

    update

--- Bazaar/Bazaar/settings.py
+++ Bazaar/Bazaar/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Bazaar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Bazaar/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Bazaar/Bazaar/urls.py
+++ Bazaar/Bazaar/urls.py
@@ -1,7 +1,6 @@
-"""Bazaar URL Configuration
-
+"""mac URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,12 +13,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shop/', include('shop.urls'))
+    path('shop/', include('shop.urls')),
+    path('blog/', include('blog.urls')),
+    path('', views.index)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
